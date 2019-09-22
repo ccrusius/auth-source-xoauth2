dfea3d867c7cd9db08f9358ffd017fd3d5bd7cc5
Cesar Crusius <cesar.crusius@stanfordalumni.org>
2019-09-22 15:57:26 -0700

    Enable lexical binding.

--- auth-source-xoauth2.el
+++ auth-source-xoauth2.el
@@ -1,4 +1,4 @@
-;;; auth-source-xoauth2.el --- Integrate auth-source with XOAUTH2
+;;; auth-source-xoauth2.el --- Integrate auth-source with XOAUTH2  -*- lexical-binding: t; -*-
 
 ;; Copyright 2018 Google LLC
 ;;
